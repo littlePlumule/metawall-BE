faab477449c8686236d66d893da6d62c7a62a2d9
hung <ff789456741@gmail.com>
2022-06-23 19:04:33 +0800

    add: isAuth Middleware

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/userController');
+const { isAuth } = require('../service/auth');
 const { catchAsync } = require('../service/errorHandler');
 
 // 註冊
@@ -10,15 +11,15 @@ router.post('/register', catchAsync(userControllers.signup));
 router.post('/login', catchAsync(userControllers.signin));
 
 // 修改密碼
-router.patch('/password', catchAsync(userControllers.updatePassword));
+router.patch('/password', isAuth, catchAsync(userControllers.updatePassword));
 
 // 取得個人頁面
-router.get('/profile', catchAsync(userControllers.getMyProfile));
+router.get('/profile', isAuth, catchAsync(userControllers.getMyProfile));
 
 // 取得他人個人頁面
-router.get('/profile/:userId', catchAsync(userControllers.getOtherProfile));
+router.get('/profile/:userId', isAuth, catchAsync(userControllers.getOtherProfile));
 
 // 編輯個人資料
-router.patch('/profile', catchAsync(userControllers.updateProfile));
+router.patch('/profile', isAuth, catchAsync(userControllers.updateProfile));
 
 module.exports = router;
