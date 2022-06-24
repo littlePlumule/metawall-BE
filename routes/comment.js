8d6d0b7e04ab90aaddedc7bf52f93f83489b075b
hung <ff789456741@gmail.com>
2022-06-24 18:41:46 +0800

    add: checkPostId & checkCommnetId & isAuth

--- routes/comment.js
+++ routes/comment.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const commentControllers = require('../controllers/commentController');
 const { catchAsync } = require('../service/errorHandler');
+const { isAuth } = require('../service/auth');
+const { checkPostId, checkCommentId } = require('../service/checkId');
 
 // 新增單一留言
-router.post('/', catchAsync(commentControllers.addComment));
+router.post('/:postId', checkPostId, isAuth, catchAsync(commentControllers.addComment));
 
 // 編輯單一留言
-router.patch('/:commentId', catchAsync(commentControllers.updateCommnet));
+router.patch('/:commentId', checkCommentId, isAuth, catchAsync(commentControllers.updateCommnet));
 
 // 刪除單一留言
-router.delete('/:commentId', catchAsync(commentControllers.deleteComment));
+router.delete('/:commentId', checkCommentId, isAuth, catchAsync(commentControllers.deleteComment));
 
 module.exports = router;
