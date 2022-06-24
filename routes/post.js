31f714a48e436e1debfe6f45485b30cac9ee1ee2
hung <ff789456741@gmail.com>
2022-06-24 16:34:29 +0800

    add: postRouter

--- routes/post.js
+++ routes/post.js
@@ -2,20 +2,25 @@ const express = require('express');
 const router = express.Router();
 const postControllers = require('../controllers/postController');
 const { catchAsync } = require('../service/errorHandler');
+const { isAuth } = require('../service/auth');
+const { checkPostId, checkUserId } = require('../service/checkId');
 
 // 取得所有貼文 
-router.get('/posts', catchAsync(postControllers.getPosts));
+router.get('/posts', isAuth, catchAsync(postControllers.getAllPosts));
+
+// 取得個人所有貼文 
+router.get('/posts/:userId', checkUserId, isAuth, catchAsync(postControllers.getPosts));
 
 // 取得單一貼文
-router.get('/post/:postId', catchAsync(postControllers.getPost));
+router.get('/post/:postId', checkPostId, isAuth, catchAsync(postControllers.getPost));
 
 // 新增單一貼文
-router.post('/post', catchAsync(postControllers.createdPost));
+router.post('/post', isAuth, catchAsync(postControllers.createdPost));
 
 // 編輯單一貼文
-router.patch('/post/:postId', catchAsync(postControllers.updatePost));
+router.patch('/post/:postId', checkPostId, isAuth, catchAsync(postControllers.updatePost));
 
 // 刪除單一貼文
-router.delete('/post/postId', catchAsync(postControllers.deletePost));
+router.delete('/post/postId', checkPostId, isAuth, catchAsync(postControllers.deletePost));
 
 module.exports = router;
