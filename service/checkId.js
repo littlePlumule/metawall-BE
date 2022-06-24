fe3f424c20cb050e09a427a7d0bfd7a0e0e412f1
hung <ff789456741@gmail.com>
2022-06-24 16:29:36 +0800

    add: checkId Middleware

---
+++ service/checkId.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { appError } = require('./errorHandler');
+const User = require('../models/userModel');
+const Post = require('../models/postModel');
+
+const checkUserId = async (req, res, next) => {
+  const { userId } = req.params;
+  const isValid = mongoose.Types.ObjectId.isValid(userId);
+  if (!isValid) {
+    return next(appError(400, 3, 'ID 格式錯誤，請重新確認'));
+  }
+  const user = await User.findById(userId);
+  if (!user) {
+    return next(appError(400, 3, '無此用戶'));
+  }
+  next()
+}
+
+const checkPostId = async (req, res, next) => {
+  const { postId } = req.params;
+  const isValid = mongoose.Types.ObjectId.isValid(postId);
+  if (!isValid) {
+    return next(appError(400, 3, 'ID 格式錯誤，請重新確認'));
+  }
+  const post = await Post.findById(postId);
+  if (!post) {
+    return next(appError(400, 3, '無此篇貼文'));
+  }
+  next()
+}
+
+module.exports = {
+  checkUserId,
+  checkPostId
+}
