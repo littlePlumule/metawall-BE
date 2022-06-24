3d65aa595e13341ed152b696f6291795ad60a40a
hung <ff789456741@gmail.com>
2022-06-24 17:00:22 +0800

    feat: updatePost

--- controllers/postController.js
+++ controllers/postController.js
@@ -114,7 +114,33 @@ const posts = {
   },
 
   async updatePost(req, res, next) {
-    
+    const { postId } = req.params;
+    let { content, image } = req.body;
+
+    content = content ? content.trim() : content;
+    image = image ? image.trim() : image;
+
+    if (!isNotEmpty({ content }).valid) {
+      return next(appError(400, 1, isNotEmpty({ content }).msg));
+    }
+
+    const post = await Post.findById(postId);
+    if (post.editor.toString() !== req.user.id) {
+      return next(appError(400, 5, { user_id: '您並非發文者，無法修改此貼文' }));
+    }
+
+    const param = {
+      content
+    };
+
+    if (image) {
+      param.image = image;
+    }
+
+    await Post.findByIdAndUpdate(postId, param);
+
+    const updatePost = await Post.findById(postId);
+    httpResponse(res, updatePost);
   },
 
   async deletePost(req, res, next) {
