82740c6b56ba2e936461c0794bdbf44ead473a8e
hung <ff789456741@gmail.com>
2022-06-24 18:38:38 +0800

    feat: checkCommentId

--- service/checkId.js
+++ service/checkId.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { appError } = require('./errorHandler');
 const User = require('../models/userModel');
 const Post = require('../models/postModel');
+const Comment = require('../models/commentModels');
 
 const checkUserId = async (req, res, next) => {
   const { userId } = req.params;
@@ -29,7 +30,21 @@ const checkPostId = async (req, res, next) => {
   next()
 }
 
+const checkCommentId = async (req, res, next) => {
+  const { commentId } = req.params;
+  const isValid = mongoose.Types.ObjectId.isValid(commentId);
+  if (!isValid) {
+    return next(appError(400, 3, 'ID 格式錯誤，請重新確認'));
+  }
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    return next(appError(400, 3, '無此留言'));
+  }
+  next()
+}
+
 module.exports = {
   checkUserId,
-  checkPostId
+  checkPostId,
+  checkCommentId
 }
