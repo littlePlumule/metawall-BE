8a4f8bfc7ea58dcaa731eb00935998b63b4178a1
hung <ff789456741@gmail.com>
2022-06-24 18:52:13 +0800

    feat: addComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,18 +1,39 @@
-const Post = require('../models/postModel');
 const Comment = require('../models/commentModels');
 const { appError } = require('../service/errorHandler');
 const httpResponse = require('../service//resHandle');
+const { isNotEmpty } = require('../service/validator');
 
 const comments = {
-  async addComment() {
+  async addComment(req, res, next) {
+    let { comment, image } = req.body;
+    const { postId } = req.params;
 
+    comment = comment ? comment.trim() : comment;
+    image = image ? image.trim() : image;
+
+    if (!isNotEmpty({ comment }).valid) {
+      return next(appError(400, 1, isNotEmpty({ comment }).msg));
+    }
+
+    const param = {
+      editor: req.user.id,
+      post: postId,
+      comment,
+    };
+
+    if (image) {
+      param.image = image;
+    }
+
+    const newComment = await Comment.create(param);
+    httpResponse(res, newComment);
   },
 
-  async updateCommnet() {
-
+  async updateCommnet(req, res, next) {
+    
   },
 
-  async deleteComment() {
+  async deleteComment(req, res, next) {
     
   }
 };
