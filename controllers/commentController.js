2523363911c95f8d4e2d186850190f92ceba479f
hung <ff789456741@gmail.com>
2022-06-24 18:52:45 +0800

    feat: updateComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -30,7 +30,33 @@ const comments = {
   },
 
   async updateCommnet(req, res, next) {
-    
+    let { comment, image } = req.body;
+    const { commentId } = req.params;
+
+    comment = comment ? comment.trim() : comment;
+    image = image ? image.trim() : image;
+
+    if (!isNotEmpty({ comment }).valid) {
+      return next(appError(400, 1, isNotEmpty({ comment }).msg));
+    }
+
+    const material = await Comment.findById(commentId);
+    if (material.editor._id.toString() !== req.user.id) {
+      return next(appError(400, 5, { user_id: '您並非留言者，無法修改此留言' }));
+    }
+
+    const param = {
+      comment
+    };
+
+    if (image) {
+      param.image = image;
+    }
+
+    await Comment.findByIdAndUpdate(commentId, param);
+
+    const updateComment = await Comment.findById(commentId);
+    httpResponse(res, updateComment);
   },
 
   async deleteComment(req, res, next) {
