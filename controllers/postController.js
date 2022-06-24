5281768d30fee8ffdee5a7986a5fb57390c0b08a
hung <ff789456741@gmail.com>
2022-06-24 16:43:48 +0800

    feat: getPosts

--- controllers/postController.js
+++ controllers/postController.js
@@ -46,7 +46,41 @@ const posts = {
   },
 
   async getPosts(req, res, next) {
-    
+    const timeSort = req.query.sort === 'asc' ? 'createdAt' : '-createdAt';
+    const filter = req.query.q !== undefined ? { content: new RegExp(req.query.q) } : {};
+    filter.editor = req.params.userId;
+    const currentPage = isValidPage(req.query.page);
+    const perPage = isValidLimit(req.query.limit);
+    const total = await Post.find(filter).count();
+    const totalPage = Math.ceil(total / perPage);
+    const skip = (currentPage - 1) * perPage;
+
+    if (totalPage > 0 && currentPage > totalPage) {
+      return next(appError(400, 1, `請輸入正確頁碼，共有${totalPage}頁`));
+    }
+
+    const posts = await Post
+      .find(filter)
+      .populate({
+        path: 'editor',
+        select: 'nickName avatar'
+      })
+      .populate({
+        path: 'comments',
+        select: 'editor comment image createdAt'
+      })
+      .sort(timeSort)
+      .skip(skip);
+
+    httpResponse(res, {
+      page: {
+        currentPage,
+        perPage,
+        total,
+        totalPage
+      },
+      list: posts
+    });
   },
 
   async getPost(req, res, next) {
