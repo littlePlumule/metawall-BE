9ca61f2ecc3eddc15f2545cd7dc25044d5f2b299
hung <ff789456741@gmail.com>
2022-06-27 14:03:02 +0800

    add: router Middleware

--- routes/likes.js
+++ routes/likes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 const likeControllers = require('../controllers/likeController');
 const { catchAsync } = require('../service/errorHandler');
+const { isAuth } = require('../service/auth');
+const { checkPostId } = require('../service/checkId');
+
 
 // 取得所有收藏
-router.get('/likes', catchAsync(likeControllers.getLikes));
+router.get('/likes', isAuth, catchAsync(likeControllers.getLikes));
 
 // 按讚
-router.post('/like/:postId', catchAsync(likeControllers.addLike));
+router.post('/like/:postId', checkPostId, isAuth, catchAsync(likeControllers.addLike));
 
 // 取消按讚
-router.delete('/like/:postId', catchAsync(likeControllers.deleteLike));
+router.delete('/unlike/:postId', checkPostId, isAuth, catchAsync(likeControllers.deleteLike));
 
 module.exports = router;
