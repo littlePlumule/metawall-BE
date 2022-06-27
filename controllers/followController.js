fd97a10c8563094d71368396f906fc1d2cc45e42
hung <ff789456741@gmail.com>
2022-06-27 13:43:57 +0800

    feat: getFollows

--- controllers/followController.js
+++ controllers/followController.js
@@ -3,15 +3,22 @@ const { appError } = require('../service/errorHandler');
 const httpResponse = require('../service/resHandle');
 
 const follows = {
-  async getFollows() {
+  async getFollows(req, res, next) {
+    const list = await User.find({
+      _id: req.user.id
+    }).populate({
+      path: 'followings.user',
+      select: 'nickName avatar _id'
+    }).select('followings').sort('followings.createdAt');
 
+    httpResponse(res, { list: list[0].followings });
   },
 
-  async addFollow() {
-
+  async addFollow(req, res, next) {
+    
   },
 
-  async deleteFollow() {
+  async deleteFollow(req, res, next) {
     
   }
 };
